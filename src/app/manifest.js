fd42b0500a9e3ec69709d2a9cc3796b7bed664dc
ripon20244 <117757122+ripon20244@users.noreply.github.com>
2024-03-24 15:31:57 +0600

    update manifest

---
+++ src/app/manifest.js
@@ -0,0 +1,18 @@
+export default function manifest() {
+    return {
+      name: 'Blockchain & Mern Stack Webapp Developer: The Future of Web Development',
+      short_name: 'BMSWD',
+      description: "Welcome to my profile, where I combine the power of blockchain technology with the versatility of MERN stack development to revolutionize the industry. Our mission is to provide secure, transparent, and efficient blockchain solutions using Solidity, Node.js, React.js, Next.js, Express.js, Remix, and MongoDB.",
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#00b8c8',
+      theme_color: '#00b8c8',
+      icons: [
+        {
+          src: '/favicon.ico',
+          sizes: 'any',
+          type: 'image/x-icon',
+        },
+      ],
+    }
+  }
