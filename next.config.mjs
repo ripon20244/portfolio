17e03c5b4f24adf2f9d661f42ac14a010c3f9ccc
ripon20244 <117757122+ripon20244@users.noreply.github.com>
2024-03-24 15:00:58 +0600

    updte config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: config => {
+      config.externals.push('pino-pretty', 'lokijs', 'encoding')
+      return config
+    },
+    // output:"export",
+    distDir: 'build',
+    trailingSlash: true,
+    httpAgentOptions: { keepAlive: true },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    images: {
+      unoptimized: true,
+    },
+  
+
+    compiler: {
+      removeConsole: {
+        // exclude: ["error"],
+      },
+    },
+  
+  
+  };
+  
 
 export default nextConfig;
