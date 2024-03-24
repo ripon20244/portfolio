67a06d767905ae3d23caf05aefd485ed7598b999
ripon20244 <117757122+ripon20244@users.noreply.github.com>
2024-03-24 15:58:59 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 
-export default function Home() {
-  return (
-  <>
-  <h1>Home</h1>
-  </>
-  );
-}
+  export default function Home() {
+    return (
+    <>
+    <h1>Home</h1>
+    </>
+    );
+  }
---
+++ src/app/portfolio/page.jsx
@@ -0,0 +1,8 @@
+
+export default function page() {
+    return (
+    <>
+    <h1>portfolio</h1>
+    </>
+    );
+  }
